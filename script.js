c4b1b4a55a98abc914702dda3da8d511c88c134d
Lelouch223311 <foracademy228@mail.ru>
2023-09-29 16:02:06 +0300

    DZ

---
+++ script.js
@@ -0,0 +1,83 @@
+
+/*
+// ==================== [ Жанры Музыки ] =====================
+let styles = ["Джаз","Блюз"]
+console.log(styles)
+
+styles.push("Рок-н-ролл")
+console.log(styles)
+
+styles[1] = "Классика"
+console.log(styles)
+
+styles.shift("Джаз") // styles.shift(0)
+console.log("Джаз") // => :3
+
+styles.unshift("Рэп","Регги")
+console.log(styles)
+
+// ==================== [ Жанры Музыки ] =====================
+*/
+/*
+function sumInput() {
+    // let cifr = prompt("Введите значение !", '5')
+    let arr = []
+    let itog;
+    if(!false){
+        itog = arr.lenght
+        console.log(`Элементов массива: ${itog}`)
+        let cifr = prompt("Введите значение !", '5')
+        for (cifr = Number;;) {
+            
+            
+            arr++;
+            itog = arr.length
+            console.log(`arr: ${arr.length}`)
+            break;
+
+        }
+    } else{
+        for(cifr != Number;;){
+
+        itog = arr.length
+        console.log(`Элементов массива: ${itog}`) 
+        break;
+        }
+
+    }
+
+        // for (cifr != Number;;) {
+        //     arr++;
+        //     let cifr = prompt("Введите значение !", '5')
+        //     arr = +cifr
+        // }
+
+}
+
+sumInput()
+*/ // => Никак не врубаюсь как второе задание сделать.
+
+
+// ===================================== [ Task #2 ] ===================================
+
+
+/*
+let arr = [1,2,3,4,5,6,7,8,9]
+function getMaxSubSum(n){
+
+    for(let i = 0; i < n; i++)
+    {
+           
+        for(let j = i; j < n; j++)
+        {     
+            for(let k = i; k <= j; k++)
+            console.log(arr[k])              
+        }
+    }
+}
+
+getMaxSubSum(arr.length);
+*/
+
+
+// =========================================== [ == ] ==================================
